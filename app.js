3e866c3d1bf900ce3f0c6a42bc72216b413fb5c5
RamDhangar <93071175+RamDhangar@users.noreply.github.com>
2021-12-15 14:05:21 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 let display= document.getElementById("showResult");
 let btn=document.querySelectorAll("button");
 let result="";
+let ev="";
 let input=document.getElementById("input");
 
 function btnclick(e){
-    result+=e.value;
+    (e.value==="%") ? ev+="/100*" : ev+=e.value;
+    (e.value==="**") ?result+="^" : result+=e.value;
     input.value=result;
-
 }
 
 function showResult(){
-  input.value=eval(result);
-  result=eval(result);
+  input.value=eval(ev);
+  result=eval(ev);
 }
 
 function clearResult(){
     result=""
     input.value=""
+    ev=""
 }
 function backSpace(){
     result=result.slice(0, -1);
